c696cd8b7aaa467232f5ad0f4ce58e3db1690f99
j-kon <jaykon4me@gmail.com>
2025-04-17 13:49:28 +0100

    Refactor scripts to use specific Bitcoin CLI commands for decoding redeem scripts and creating multisig and CSV scripts with updated parameters

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,4 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
-bitcoin-cli -regtest createmultisig 1 '["02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"]' '{"blocks":150}'
+bitcoin-cli -regtest createmultisig 1 '["02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"]' '{"checksequenceverify":150}'
