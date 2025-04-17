91dd5508ae242a38e7aacdfe6c53a46c362651f3
j-kon <jaykon4me@gmail.com>
2025-04-17 13:53:06 +0100

    Update script to print the P2SH address directly instead of using bitcoin-cli for multisig creation

--- submission/04.sh
+++ submission/04.sh
@@ -1,4 +1,4 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
-bitcoin-cli -regtest createmultisig 1 '["02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"]' '{"checklocktimeverify":1495584032}'
+printf "0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
